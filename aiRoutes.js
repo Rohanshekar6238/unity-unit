5e8c1fc4dccc04e03fdfab0b3d675d16fb5685ca
Rohanshekar6238 <ragirirohanshekar@gmail.com>
2025-03-10 10:36:17 +0530

    Delete aiRoutes.js

--- aiRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAIResponse } from "../controllers/aiController.js";
-
-const router = express.Router();
-router.post("/analyze", getAIResponse);
-
-export default router;
